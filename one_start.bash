4a3ef1a0a9c7cff38866026bac2871762f5457fc
Cauchy <119838619+lkx8421@users.noreply.github.com>
2025-02-24 14:41:36 +0800

    fix: auto get tita nn and airbot interface of urdf

--- one_start.bash
+++ one_start.bash
@@ -1,11 +1,24 @@
 #!/bin/bash
-sudo echo "start"
-ros2 service call /tita3037322/robot_inertia_calculator/enable_calculate std_srvs/srv/SetBool "{data: true}"
-sudo insmod /home/robot/gs_usb.ko
+unique_id_file="/proc/device-tree/serial-number"
+tita_namespace="tita"  
+
+if [[ -f "$unique_id_file" ]]; then
+    unique_id=$(tr -d '\000' < "$unique_id_file")  
+    unique_id=${unique_id:6} 
+
+    if [[ -n "$unique_id" ]]; then
+        tita_namespace="tita${unique_id}"
+    fi
+fi
+
+echo "Tita namespace: $tita_namespace"
+
+ros2 service call /${tita_namespace}/robot_inertia_calculator/enable_calculate std_srvs/srv/SetBool "{data: true}"
+# sudo insmod /home/robot/gs_usb.ko
 # 定义变量
 IMAGE="modify_airbot"
 CONTAINER_NAME="airbot"
-WORKSPACE_DIR="/home/robot/manipulator_ws/airbot_ws"
+WORKSPACE_DIR="/home/robot/manipulator/airbot_on_tita"
 SSH_DIR="$HOME/.ssh"
 
 # 启动容器并执行命令
